46e85e63201ddd861768448c7ebc566cf0c6ca68
Kyle Koder <silvertallon@gmail.com>
2019-01-23 23:05:14 -0800

    Config settings should now save when using in-game menu

--- src/main/java/com/fantasticsource/waystoneadditions/WaystoneAdditions.java
+++ src/main/java/com/fantasticsource/waystoneadditions/WaystoneAdditions.java
@@ -1,8 +1,13 @@
 package com.fantasticsource.waystoneadditions;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = WaystoneAdditions.MODID, name = WaystoneAdditions.NAME, version = WaystoneAdditions.VERSION, dependencies = "required-after:waystones@[4.0.67,)", acceptableRemoteVersions = "*")
@@ -14,9 +19,20 @@ public class WaystoneAdditions
 
     private static Logger logger;
 
+    public WaystoneAdditions()
+    {
+        MinecraftForge.EVENT_BUS.register(WaystoneAdditions.class);
+    }
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
     }
 }
