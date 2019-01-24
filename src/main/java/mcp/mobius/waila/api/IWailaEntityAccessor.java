cc70f77806464b6731ded904e4e59ee2e542e48e
Kyle Koder <silvertallon@gmail.com>
2019-01-23 17:48:48 -0800

    Deleted large portions of the Waila API due to all sorts of compile errors
    The game now runs in the dev environment

--- src/main/java/mcp/mobius/waila/api/IWailaEntityAccessor.java
+++ src/main/java/mcp/mobius/waila/api/IWailaEntityAccessor.java
@@ -3,8 +3,6 @@ package mcp.mobius.waila.api;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 /* The Accessor is used to get some basic data out of the game without having to request
@@ -12,13 +10,17 @@ import net.minecraft.world.World;
  * It will also return things that are unmodified by the overriding systems (like getWailaStack).
  */
 
-public interface IWailaEntityAccessor {
-	World        		 getWorld();
-	EntityPlayer 		 getPlayer();
-	Entity               getEntity();
-	MovingObjectPosition getPosition();
-	Vec3                 getRenderingPosition();
-	NBTTagCompound       getNBTData();
-	int                  getNBTInteger(NBTTagCompound tag, String keyname);
-	double               getPartialFrame();
+public interface IWailaEntityAccessor
+{
+    World getWorld();
+
+    EntityPlayer getPlayer();
+
+    Entity getEntity();
+
+    NBTTagCompound getNBTData();
+
+    int getNBTInteger(NBTTagCompound tag, String keyname);
+
+    double getPartialFrame();
 }
