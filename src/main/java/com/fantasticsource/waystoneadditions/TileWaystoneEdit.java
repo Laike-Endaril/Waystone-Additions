efcd3156945c2e742603cdd24e7e5a742a59d6f8
Kyle Koder <silvertallon@gmail.com>
2019-01-24 23:50:55 -0800

    Added the rest of the TileWaystone content into TileWaystoneEdit.  This approach may lack forward compatibility, but at least it seems to be working now

--- src/main/java/com/fantasticsource/waystoneadditions/TileWaystoneEdit.java
+++ src/main/java/com/fantasticsource/waystoneadditions/TileWaystoneEdit.java
@@ -1,14 +1,23 @@
 package com.fantasticsource.waystoneadditions;
 
 import net.blay09.mods.waystones.block.TileWaystone;
+import net.blay09.mods.waystones.worldgen.NameGenerator;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTUtil;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.CapabilityDispatcher;
 import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
+import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.util.UUID;
 
@@ -83,6 +92,133 @@ public class TileWaystoneEdit extends TileWaystone
             tagCompound.setBoolean("IsMossy", isMossy);
         }
         return tagCompound;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound tagCompound)
+    {
+        super.readFromNBT(tagCompound);
+        isDummy = tagCompound.getBoolean("IsDummy");
+        if (!isDummy)
+        {
+            waystoneName = tagCompound.getString("WaystoneName");
+            wasGenerated = tagCompound.getBoolean("WasGenerated");
+            if (tagCompound.hasKey("Owner")) owner = NBTUtil.getUUIDFromTag(tagCompound.getCompoundTag("Owner"));
+
+            isGlobal = tagCompound.getBoolean("IsGlobal");
+
+            isMossy = tagCompound.getBoolean("IsMossy");
+        }
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt)
+    {
+        super.onDataPacket(net, pkt);
+        generateNameIfNecessary();
+        readFromNBT(pkt.getNbtCompound());
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag()
+    {
+        generateNameIfNecessary();
+        return writeToNBT(new NBTTagCompound());
+    }
+
+    private void generateNameIfNecessary()
+    {
+        if (waystoneName.isEmpty())
+        {
+            waystoneName = NameGenerator.get(world).getName(world.getBiome(pos), world.rand);
+        }
+    }
+
+    @Nullable
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket()
+    {
+        return new SPacketUpdateTileEntity(pos, 0, getUpdateTag());
+    }
+
+    public String getWaystoneName()
+    {
+        return waystoneName;
+    }
+
+    public boolean isOwner(EntityPlayer player)
+    {
+        return owner == null || player.getGameProfile().getId().equals(owner) || player.capabilities.isCreativeMode;
+    }
+
+    public boolean isMossy()
+    {
+        return isMossy;
+    }
+
+    public void setMossy(boolean mossy)
+    {
+        isMossy = mossy;
+    }
+
+    public boolean wasGenerated()
+    {
+        return wasGenerated;
+    }
+
+    public void setWasGenerated(boolean wasGenerated)
+    {
+        this.wasGenerated = wasGenerated;
+    }
+
+    public void setWaystoneName(String waystoneName)
+    {
+        this.waystoneName = waystoneName;
+        IBlockState state = world.getBlockState(pos);
+        world.markAndNotifyBlock(pos, world.getChunkFromBlockCoords(pos), state, state, 3);
+        markDirty();
+    }
+
+    @Override
+    public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate)
+    {
+        return oldState.getBlock() != newSate.getBlock();
+    }
+
+    @Override
+    public AxisAlignedBB getRenderBoundingBox()
+    {
+        return new AxisAlignedBB(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 2, pos.getZ() + 1);
+    }
+
+    public void setOwner(EntityPlayer owner)
+    {
+        this.owner = owner.getGameProfile().getId();
+        markDirty();
+    }
+
+    public boolean isGlobal()
+    {
+        return isGlobal;
+    }
+
+    public void setGlobal(boolean isGlobal)
+    {
+        this.isGlobal = isGlobal;
+        markDirty();
+    }
+
+    public TileWaystone getParent()
+    {
+        if (isDummy)
+        {
+            TileEntity tileBelow = world.getTileEntity(pos.down());
+            if (tileBelow instanceof TileWaystone)
+            {
+                return (TileWaystone) tileBelow;
+            }
+        }
+        return this;
     }
 
 
