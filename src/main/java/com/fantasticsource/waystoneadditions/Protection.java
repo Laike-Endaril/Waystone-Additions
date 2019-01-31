0ea98f8836552cd8838f57abba07d24d3c2213ba
Kyle Koder <silvertallon@gmail.com>
2019-01-30 18:54:55 -0800

    Block protection now prevents bucket usage

--- src/main/java/com/fantasticsource/waystoneadditions/Protection.java
+++ src/main/java/com/fantasticsource/waystoneadditions/Protection.java
@@ -5,8 +5,10 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraftforge.common.util.BlockSnapshot;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.FillBucketEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
@@ -36,11 +38,23 @@ public class Protection
     {
         //This prevents the player from making progress on breaking a block instead of preventing the breaking itself; looks and acts much cleaner this way
         //It also catches instant breaking, at least in the case of punching flowers and whatnot.  Must simply use a high break speed by default
-        //TODO Some mod items may bypass this.  Exchangers and psi tools come to mind
         if (isBuildProtected(event.getPos(), event.getEntity()))
         {
-            event.setCanceled(true);
             event.setNewSpeed(0);
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void bucketUse(FillBucketEvent event)
+    {
+        //Prevents bucket usage in the protected zones
+        //This event is not just for filling buckets; it is also for emptying them
+        RayTraceResult rayTrace = event.getTarget();
+        if (rayTrace == null || isBuildProtected(new BlockPos(rayTrace.hitVec), event.getEntity()))
+        {
+            event.setResult(Event.Result.DENY);
+            event.setCanceled(true);
         }
     }
 
@@ -48,10 +62,9 @@ public class Protection
     public static void blockInteract(PlayerInteractEvent.RightClickBlock event)
     {
         //This prevents the player from using an item.  Might not catch everything but when it works, it works nicely (it doesn't cause inventory desync)
-        //TODO For some reason, this does not block buckets' interactions with fluids!
+        //This does not detect buckets!  Buckets are handled in the FillBucketEvent (which should be named UseBucketEvent)
         if (isBuildProtected(new BlockPos(event.getHitVec()), event.getEntity()))
         {
-            System.out.println("Blocked in filter 1?");
             event.setUseItem(Event.Result.DENY);
             event.setUseBlock(Event.Result.ALLOW);
         }
@@ -63,7 +76,6 @@ public class Protection
         //Might catch some things RightClickBlock doesn't catch, but not as nice for when blocks are placed from inventory, because it causes inventory desync
         if (isBuildProtected(event.getPos(), event.getPlayer()))
         {
-            System.out.println("Blocked in filter 2");
             event.setResult(Event.Result.DENY);
             event.setCanceled(true);
         }
@@ -72,12 +84,11 @@ public class Protection
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
     public static void blockMultiPlace(BlockEvent.MultiPlaceEvent event)
     {
-        //I imagine this is similar to PlaceEvent, but for when multiple blocks are placed at once.  Might catch doors and beds in vanilla?
+        //I imagine this is similar to PlaceEvent, but for when multiple blocks are placed at once
         for (BlockSnapshot snapshot : event.getReplacedBlockSnapshots())
         {
             if (isBuildProtected(snapshot.getPos(), event.getPlayer()))
             {
-                System.out.println("Blocked in filter 3");
                 event.setResult(Event.Result.DENY);
                 event.setCanceled(true);
                 break;
